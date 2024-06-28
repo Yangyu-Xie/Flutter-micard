8b7a287e4cb90c8cd636b1efcb8f14f3fcd7a6bf
Yangyu-Xie <xlyxieyang@outlook.jp>
2024-06-28 23:52:39 +0900

    my personal implementation. I used documentation and stackoveflow to write the code so far

--- lib/main.dart
+++ lib/main.dart
@@ -14,37 +14,61 @@ class myApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.red,
-                child: Text("Hello World"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //Container for photo
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: CircleAvatar(
+                      radius: 48, // Image radius
+                      backgroundImage: NetworkImage(
+                          'https://f-photobook.jp/column/img/59/img_01.jpg'),
+                    ), //Replace this line with actual photo
+                  ),
+                  //Container for name
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0),
+                    child: Text("Yangyu Xie"),
+                  ),
+                  //Container for titles
+                  Container(
+                    child: Text("Flutter learner"),
+                  ),
+                  // Container for a while line
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    color: Colors.white,
+                    height: 1,
+                    width: 100,
+                  ),
+
+                  //phone number container
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 30,
+                        width: 200,
+                        color: Colors.white,
+                        child: Center(child: Text("+81-80-3244-6989")),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                      ),
+                      Container(
+                          height: 30,
+                          width: 200,
+                          color: Colors.white,
+                          child: Center(child: Text("xlyxieyang@outlook.jp"))),
+                    ],
+                  ),
+                ],
               ),
-              Container(
-                height: double.infinity,
-                width: 100,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 100,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      height: 100,
-                      color: Colors.green,
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.blue,
-                child: Text("Hello World"),
-              )
             ],
           ),
         ),
